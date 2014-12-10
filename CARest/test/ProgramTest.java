98374bd09e94980f725ba318265ed7092964bcce
FrederikOlesen <Frederik.o@mailme.dk>
2014-12-10 12:11:36 +0100

    Fixed the last thing about the tests. All methods are now tested.

--- CARest/test/ProgramTest.java
+++ CARest/test/ProgramTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.Before;
 import com.google.gson.Gson;
 import model.Credentials;
+import org.junit.After;
 import static org.junit.Assert.*;
 
 public class ProgramTest {
@@ -16,6 +17,9 @@ public class ProgramTest {
     private final Gson gson = new Gson();
     Facade facade;
 
+    String username = "cphtestjpa";
+    String json = "{\"username\":\"cphtestjpa\",\"password\":\"Test\",\"role\":\"STUDENT\"}";
+
     public ProgramTest() {
 
     }
@@ -23,45 +27,56 @@ public class ProgramTest {
     @Before
     public void before() {
         facade = new Facade();
-
+        //Test adding a new person.
         em.getTransaction().begin();
+        Credentials p = facade.addPersonFromGson(json);
 
+    }
+
+    @Test
+    public void testAddPerson() throws Exception {
+        facade.delete(username);
+        facade.addPersonFromGson(json);
+        final String personAsJSONPass = facade.getPersonAsJson(username);
+        assertEquals(true, personAsJSONPass.contains("{\"username\":\"cphtestjpa\",\"password\":\"Test\","
+                + "\"role\":\"STUDENT\"}"));
     }
 
     @Test
     public void testGetPerson() throws Exception {
 
-        String username = "cphtestjpa";
-        String json = "{\"username\":\"cphtestjpa\",\"password\":\"Test\",\"role\":\"STUDENT\"}";
-
-        //Test adding a new person.
-        Credentials p = gson.fromJson(json, Credentials.class);
-
-        em.persist(p);
-
-        em.getTransaction().commit();
-
         //Test retrieving the added user.
         final String personAsJson = facade.getPersonAsJson(username);
 
-        assertEquals(true, personAsJson.contains("{\"username\":\"cphtestjpa\",\"password\":\"Test\",\"role\":\"STUDENT\"}"));
+        assertEquals(true, personAsJson.contains("{\"username\":\"cphtestjpa\",\"password\":\"Test\","
+                + "\"role\":\"STUDENT\"}"));
+    }
 
+    @Test
+    public void testChangePassword() throws Exception {
         //Test to change password.
-        String password = "{\"username\":\"cphtestjpa\",\"password\":\"Test\",\"confirmedpassword\":\"Test123\"}";
+        String password = "{\"username\":\"cphtestjpa\",\"password\":\"Test\""
+                + ",\"confirmedpassword\":\"Test123\"}";
 
         facade.changePassword(password, username);
-
         final String personAsJSONPass = facade.getPersonAsJson(username);
+        assertEquals(true, personAsJSONPass.contains("{\"username\":\"cphtestjpa\",\"password\":\"Test123\""
+                + ",\"role\":\"STUDENT\"}"));
+    }
 
-        assertEquals(true, personAsJSONPass.contains("{\"username\":\"cphtestjpa\",\"password\":\"Test123\",\"role\":\"STUDENT\"}"));
-
+    @Test
+    public void testDeletePerson() {
         //Test the delete method.
         facade.delete(username);
-
         final String personAsJson1 = facade.getPersonAsJson(username);
 
-        assertEquals(false, personAsJson1.contains("{\"username\":\"cphtestjpa\",\"password\":\"Test123\",\"role\":\"STUDENT\"}"));
+        assertEquals(false, personAsJson1.contains("{\"username\":\"cphtestjpa\",\"password\":\"Test123\","
+                + "\"role\":\"STUDENT\"}"));
+    }
 
+    @After
+    public void afterDelete() {
+        facade.delete(username);
     }
 
 }
