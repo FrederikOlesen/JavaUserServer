d44b820127bbcce487419319f91d0d2c4019037f
FrederikOlesen <Frederik.o@mailme.dk>
2015-02-26 14:29:42 +0100

    Created RestAPI.

---
+++ CARest/build/generated-sources/ap-source-output/model/Credentials_.java
@@ -0,0 +1,15 @@
+package model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-02-26T14:16:42")
+@StaticMetamodel(Credentials.class)
+public class Credentials_ { 
+
+    public static volatile SingularAttribute<Credentials, String> password;
+    public static volatile SingularAttribute<Credentials, String> role;
+    public static volatile SingularAttribute<Credentials, String> username;
+
+}
